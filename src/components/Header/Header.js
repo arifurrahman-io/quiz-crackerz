7cfce72ebc08d2bc7f968f3ff17f8633cd1dacc4
arifurrahman01 <arifurrahman.now@gmail.com>
2022-10-12 23:43:18 +0600

    responsive navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Bars4Icon, XMarkIcon } from '@heroicons/react/24/solid'
 import './Header.css'
 
 const Header = () => {
+
+
+    const [open, setOpen] = useState(false);
+
+
+
     return (
-        <div className='header  bg-purple-600 text-base lg:text-2xl font-bold text-white py-3'>
-            <div>
-                <h2><Link to='/'>Tech Quiz</Link></h2>
+        <nav className='bg-purple-500'>
+            <div onClick={() => setOpen(!open)} className="h-10 w-10 md:hidden">
+                {
+                    open ? <XMarkIcon className='text-white ' /> : <Bars4Icon className='text-white' />
+                }
             </div>
-            <div className='header-link'>
-                <Link to='/'>Home</Link>
-                <Link to='/statistics'>Statistics</Link>
-                <Link to='/blog'>Blog</Link>
+
+            <div className={`md:flex bg-purple-500 w-full mr-10 justify-around text-xl text-white py-4 absolute md:static ease-in-out duration-1000 ${open ? 'top-10' : 'top-[-180px]'}`}>
+                <Link to='/' className='px-5 md:px-0 font-bold text-xl md:text-3xl text-orange-400'>Tech Quiz</Link>
+                <div className='flex flex-col md:flex-row justify-center align-middle px-3 md:px-0'>
+                    <Link to='/' className='mx-2 font-bold text-xl md:text-3xl'>Home</Link>
+                    <Link to='/statistics' className='mx-2 font-bold text-xl md:text-3xl'>Statistics</Link>
+                    <Link to='/blog' className='mx-2 font-bold text-xl md:text-3xl'>Blog</Link>
+                </div>
             </div>
-        </div>
+
+        </nav>
     );
 };
 
